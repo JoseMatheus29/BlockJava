5eefdc43a5c482cf1eaa73d8bd85c9cacf98141c
JoseMatheus29 <mateus.alvino.101@gmail.com>
2025-04-07 16:46:12 -0300

    Primeiro Commit

---
+++ src/main/java/com/matheus/blockchain/BlockJava/BlockJavaApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.blockchain.BlockJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/blockchain/BlockJava/BlockJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.blockchain.BlockJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
